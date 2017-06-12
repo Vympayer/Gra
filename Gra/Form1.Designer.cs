77ddd6aca39b3a8753efb95d3f99d6c475e6dbc6
Patrk Klusek <vympayer23@gmail.com>
2017-06-12 15:34:59 +0200

    Picture box i przycisk nowa gra

--- Gra/Form1.Designer.cs
+++ Gra/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Nowa_Gra = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(27, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1115, 723);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Nowa_Gra
+            // 
+            this.Nowa_Gra.Location = new System.Drawing.Point(1181, 28);
+            this.Nowa_Gra.Name = "Nowa_Gra";
+            this.Nowa_Gra.Size = new System.Drawing.Size(242, 105);
+            this.Nowa_Gra.TabIndex = 1;
+            this.Nowa_Gra.Text = "Nowa Gra";
+            this.Nowa_Gra.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1477, 769);
+            this.Controls.Add(this.Nowa_Gra);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Nowa_Gra;
     }
 }
 
