739a6123436dc65c11adae50e9dbde4837ac398a
Patrk Klusek <vympayer23@gmail.com>
2017-06-12 18:46:25 +0200

    Funkcja ustawiajaca cegielki

--- Gra/Figury.cs
+++ Gra/Figury.cs
@@ -8,37 +8,36 @@ namespace Gra
 {
     class Figury
     {
-        private
         int pos_x;
         int pos_y;
         int height;
         int width;
-        public
-            int get_pos_x()
+
+        public int get_pos_x()
         {
             return pos_x;
         }
-        int get_pos_y()
+        public int get_pos_y()
         {
             return pos_y;
         }
-        int get_height()
+        public int get_height()
         { return height; }
-        int get_width()
+        public int get_width()
         { return width; }
-        void set_pos_x(int a)
+        public void set_pos_x(int a)
         {
             pos_x = a;
         }
-        void set_pos_y(int a)
+        public void set_pos_y(int a)
         {
             pos_y = a;
         }
-        void set_height(int a)
+        public void set_height(int a)
         {
             height = a;
         }
-        void set_width(int a)
+        public void set_width(int a)
         {
             width = a;
         }
--- Gra/Form1.cs
+++ Gra/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,10 +8,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 namespace Gra
 {
     public partial class Form1 : Form
     {
+        private Figury[] Bricks = new Figury[28];
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +22,19 @@ namespace Gra
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+        private void generate_bricks(Figury[] a)
+        { int LICZBA_RZEDOW = 4;
+            int ELEMENT_RZAD = 7;
+            int BRICk_HEIGHT = 10;
+            int BRICK_WIDTH = 20;
+
+            for (int i = 0; i < LICZBA_RZEDOW; i++)
+                for (int j = 0; j < ELEMENT_RZAD; j++)
+                { a[i + j].set_height(BRICk_HEIGHT);
+                    a[i + j].set_width(BRICK_WIDTH);
+
+                }
         }
     }
 }
