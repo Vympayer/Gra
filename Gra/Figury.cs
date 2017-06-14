c68eeff556e4e5181be96b0dc47a0d31f948ca7e
Patrk Klusek <vympayer23@gmail.com>
2017-06-14 16:51:24 +0200

    Zmiany w klasie figury

--- Gra/Figury.cs
+++ Gra/Figury.cs
@@ -12,11 +12,13 @@ namespace Gra
         int pos_y;
         int height;
         int width;
-        public int right;
-        public bool moving;
+        bool moving;
+        int up;
+        int right;
         
-        public Figury(int a, int b, int c, int d) { pos_x = a; pos_y = b;height = c;width = d; right = 0; moving = false; }
-        public Figury() { pos_x = 10; pos_y = 10; height = 10; width = 10; right = 0; moving = false; }
+        
+        public Figury(int a, int b, int c, int d, bool e, int f,int g) { pos_x = a; pos_y = b;height = c;width = d; right = f; moving = e; up = g; }
+        public Figury() { pos_x = 10; pos_y = 10; height = 10; width = 10; right = 0; moving = false; up = 0; }
 
         public int get_pos_x()
         {
@@ -46,5 +48,11 @@ namespace Gra
         {
             width = a;
         }
+        public bool get_moving() { return moving; }
+        public int get_up() { return up; }
+        public int get_right() { return right; }
+        public void set_moving(bool a) { moving = a; }
+        public void set_up(int a) { up = a; }
+        public void set_right(int a) { right = a; }
     }
 }
